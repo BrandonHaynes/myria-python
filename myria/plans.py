36422bc4b1f156b44ec395fc98ec44145e7e2b1c
Brandon Haynes <bhaynes@cs.washington.edu>
2015-04-19 13:59:50 -0700

    Initial work on JSONPath'd plans and higher-level plan classes

--- myria/plans.py
+++ myria/plans.py
@@ -1,10 +1,123 @@
 """ Utilities for generating Myria plans """
 
 from functools import partial
-
+import jsonpath_rw as jsonpath
 
 DEFAULT_SCAN_TYPE = 'FileScan'
 DEFAULT_INSERT_TYPE = 'DbInsert'
+
+
+class MyriaPlan(object):
+    expressions = {
+        'fragments': jsonpath.parse('$.plan.fragments.[*]'),
+        'operators': jsonpath.parse('$.plan.fragments.[*].operators.[*]')
+        }
+
+    def __init__(self, json):
+        self.json = json
+
+    @property
+    def type(self):
+        return self.json['plan']['type']
+
+    @property
+    def language(self):
+        return self.json['language']
+
+    @property
+    def profiling_mode(self):
+        return self.json['profilingMode']
+
+    @property
+    def text(self):
+        return self.json['rawQuery']
+
+    @property
+    def logicalRa(self):
+        return self.json['logicalRa']
+
+    @property
+    def fragments(self):
+        return (MyriaFragment(f.value, f.path, f.context, self) for f in self.expressions['fragments'].find(self.json))
+
+    @property
+    def operators(self):
+        return (MyriaOperator(o.value, o.path, o.context, self) for o in self.expressions['operators'].find(self.json))
+
+    def __str__(self):
+        return "MyriaPlan(%s)" % (self.text or self.json)
+
+    def __repr__(self):
+        return self.__str__()
+
+
+class MyriaFragment(jsonpath.DatumInContext):
+    expressions = {
+        'operators': jsonpath.parse('`this`.operators[*]')
+        }
+
+    def __init__(self, value, path=None, context=None, plan=None):
+        super(MyriaFragment, self).__init__(value, path, context)
+        self.plan = plan
+
+    @property
+    def workers(self):
+        return self.value['overrideWorkers']
+
+    @property
+    def operators(self):
+        return (MyriaOperator(o.value, o.path, o.context, self.plan) for o in self.expressions['operators'].find(self))
+
+
+class MyriaOperator(jsonpath.DatumInContext):
+    def __init__(self, value, path=None, context=None, plan=None):
+        super(MyriaOperator, self).__init__(value, path, context)
+        self.plan = plan
+
+    @property
+    def id(self):
+        return self.value['opId']
+
+    @property
+    def name(self):
+        return self.value['opName']
+
+    @property
+    def type(self):
+        return self.value['opType']
+
+    @property
+    def fragment(self):
+        return self.context.context
+
+    @property
+    def parent(self):
+        return next((op for op in self.plan.operators
+                        for child in op.children
+                        if self.id == child.id), None)
+
+    @property
+    def children(self):
+        return (op for op in self.plan.operators
+                   if op.id in self._child_ids)
+
+    @property
+    def _child_ids(self):
+        # Is this better than explicitly enumerating child attribute names?
+        return (value for key, value in self.items()
+                      if 'CHILD' in key.upper() and
+                         isinstance(value, int))
+
+    def add(self, key, value): self.value.add(key, value)
+    def items(self): return self.value.items()
+    def keys(self): return self.value.keys()
+    def values(self): return self.value.values()
+    def __len__(self): return len(self.value)
+    def __iter__(self): return self.value.__iter__()
+    def __getitem__(self, item): return self.value[item]
+    def __setitem__(self, item, value): self.value[item] = value
+    def __delitem__(self, key): del self.value[key]
+    def __contains__(self, item): return self.value.contains(item)
 
 
 def get_parallel_import_plan(schema, work, relation, text='',
